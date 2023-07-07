91110c3b178d211f40b17286863cbacb602a870f
vitalij99 <vitalisklymko@gmail.com>
2023-07-07 21:22:45 +0300

    add auth  redax and persistor

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from 'components/App';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loader from 'components/Loader/Loader';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate persistor={persistor} loading={<Loader />}> */}
-      <BrowserRouter basename="/recipes5">
-        <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
+      <PersistGate persistor={persistor} loading={<Loader />}>
+        <BrowserRouter basename="/recipes5">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,62 @@
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { signinThunk, logoutThunk, registerThunk } from './authOperation';
+
+const STATUS = {
+  PENDING: 'pending',
+  FULFILLED: 'fulfilled',
+  REJECTED: 'rejected',
+};
+
+const initialState = {
+  user: {
+    name: null,
+    email: null,
+  },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+  isLoading: false,
+  error: null,
+};
+const arrThunks = [registerThunk, signinThunk, logoutThunk];
+
+const fn = type => arrThunks.map(el => el[type]);
+
+const handleIsLoggedIn = (state, { payload }) => {
+  state.user = payload.user;
+  state.token = payload.token;
+  state.isLoggedIn = true;
+};
+const handleLogout = state => {
+  state.user = initialState.user;
+  state.token = initialState.token;
+  state.isLoggedIn = false;
+};
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+const handleFulfilled = state => {
+  state.isLoading = false;
+  state.error = '';
+};
+const handleRejected = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = payload;
+};
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: builder => {
+    const { PENDING, FULFILLED, REJECTED } = STATUS;
+    builder
+      .addCase(registerThunk.fulfilled, handleIsLoggedIn)
+      .addCase(signinThunk.fulfilled, handleIsLoggedIn)
+      .addCase(logoutThunk.pending, handleLogout)
+      .addMatcher(isAnyOf(...fn(PENDING)), handlePending)
+      .addMatcher(isAnyOf(...fn(REJECTED)), handleRejected)
+      .addMatcher(isAnyOf(...fn(FULFILLED)), handleFulfilled);
+  },
+});
+
+export const authReducer = authSlice.reducer;
