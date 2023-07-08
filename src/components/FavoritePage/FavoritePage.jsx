4e493aa7bc0c6dbdd17c5c49012c6960a898c4ce
Umberban <romanovborysv@gmail.com>
2023-07-08 23:47:56 +0300

    add basic render
    Зроблена начальна розмітка для сторінки favorite. Її же можно використовувати для сторінки my recipes.
    Лишилось зробить пагінацію, і прикрути запити для отримання і видалення

---
+++ src/components/FavoritePage/FavoritePage.jsx
@@ -0,0 +1,61 @@
+import {
+    Card,
+    CardImg,
+    CardDesc,
+    CardList,
+    CardTimeWrap,
+    CardTextWrap,
+    CardTitle,
+    PageTitle,
+    TrashSvg,
+    Time,
+    DeleteBtn,
+    SeeRecipeBtn,
+    ErrorPhoto,
+    ErrorWrap,
+  } from './FavoritePage.styled';
+  import Container from 'components/Container/Container';
+  import { Link } from "react-router-dom";
+  import someJson  from './testarray';
+  export const FavoritePage = ({var_title}) => {
+  
+  
+    const FavoriteCards = someJson.map(
+      ({ _id, title, description, preview, time }) => (
+        <Card key={_id}>
+          <CardImg src={preview} alt="dish" />
+          <CardTextWrap>
+            <div>
+              <CardTitle>{title}</CardTitle>
+              <DeleteBtn>
+                <TrashSvg />
+              </DeleteBtn>
+            </div>
+            <CardDesc>{description}</CardDesc>
+            <CardTimeWrap>
+              <Time>{time} min</Time>
+              <Link
+                to={`/recipe/${_id}`}
+              >
+                <SeeRecipeBtn>See recipee</SeeRecipeBtn>
+              </Link>
+            </CardTimeWrap>
+          </CardTextWrap>
+        </Card>
+      )
+    );
+  
+    return (
+        <Container>
+        <PageTitle>{var_title}</PageTitle>
+        {someJson.length !== 0 ? (
+            <CardList>
+            {FavoriteCards}
+          </CardList>
+        ) : (<ErrorWrap>
+            <ErrorPhoto></ErrorPhoto>
+            </ErrorWrap>
+            )}
+        </Container>
+    );
+  };
