31bf3aa54dac2a354393026d30acbc1498f10c66
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-07-09 17:30:09 +0300

    addRecipe styledMobile

---
+++ src/components/AddRecipe/RecipePreparation/RecipePreparation.styled.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Title = styled.h3`
+color: #3E4462;
+font-family: Poppins;
+font-size: 24px;
+font-style: normal;
+font-weight: 600;
+line-height: 24px;
+letter-spacing: -0.48px;
+margin-bottom:24px
+
+`
+export const TextArea = styled.textarea`
+width: 343px;
+height: 154px;
+resize: none;
+padding: 10px 16px;
+outline: none;
+background-color: rgb(217, 217, 217);
+border: none;
+border-radius: 6px;
+margin-bottom: 18px;
+box-sizing: border-box;
+color: rgb(0, 0, 0);
+`
