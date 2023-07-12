41f6c2eefe39efd4b2ad5dd91bb431eb8143588c
NVZ1994 <nikita.zhuk.1994@gmail.com>
2023-07-12 18:12:32 +0300

    commented loader

--- src/page/AddRecipePage/AddRecipePage.jsx
+++ src/page/AddRecipePage/AddRecipePage.jsx
@@ -1,32 +1,32 @@
-// import React from 'react';
-// import AddRecipeForm from 'components/AddRecipe/AddRecipeForm';
-// import MainPageTitle from 'components/MainPageTitle/MainPageTitle';
-
-// const AddRecipePage = () => {
-//   return (
-//     <div>
-//       <MainPageTitle title={'Add recipe'} />
-//       <AddRecipeForm />
-//     </div>
-//   );
-// };
-
-// export default AddRecipePage;
-
-import React, { useState } from 'react';
+import React from 'react';
 import AddRecipeForm from 'components/AddRecipe/AddRecipeForm';
 import MainPageTitle from 'components/MainPageTitle/MainPageTitle';
-import Loader from 'components/Loader/Loader';
 
 const AddRecipePage = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
   return (
     <div>
       <MainPageTitle title={'Add recipe'} />
-      {isLoading ? <Loader /> : <AddRecipeForm />}
+      <AddRecipeForm />
     </div>
   );
 };
 
 export default AddRecipePage;
+
+// import React, { useState } from 'react';
+// import AddRecipeForm from 'components/AddRecipe/AddRecipeForm';
+// import MainPageTitle from 'components/MainPageTitle/MainPageTitle';
+// import Loader from 'components/Loader/Loader';
+
+// const AddRecipePage = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   return (
+//     <div>
+//       <MainPageTitle title={'Add recipe'} />
+//       {isLoading ? <Loader /> : <AddRecipeForm />}
+//     </div>
+//   );
+// };
+
+// export default AddRecipePage;
