ce30c4a3c0db7177fbee37f3947adc1d718b0692
NVZ1994 <nikita.zhuk.1994@gmail.com>
2023-07-12 18:06:50 +0300

    added loader and styles

--- src/page/AddRecipePage/AddRecipePage.jsx
+++ src/page/AddRecipePage/AddRecipePage.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
+// import React from 'react';
+// import AddRecipeForm from 'components/AddRecipe/AddRecipeForm';
+// import MainPageTitle from 'components/MainPageTitle/MainPageTitle';
+
+// const AddRecipePage = () => {
+//   return (
+//     <div>
+//       <MainPageTitle title={'Add recipe'} />
+//       <AddRecipeForm />
+//     </div>
+//   );
+// };
+
+// export default AddRecipePage;
+
+import React, { useState } from 'react';
 import AddRecipeForm from 'components/AddRecipe/AddRecipeForm';
 import MainPageTitle from 'components/MainPageTitle/MainPageTitle';
+import Loader from 'components/Loader/Loader';
 
 const AddRecipePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div>
       <MainPageTitle title={'Add recipe'} />
-      <AddRecipeForm />
+      {isLoading ? <Loader /> : <AddRecipeForm />}
     </div>
   );
 };
 
 export default AddRecipePage;
-
-// import React from 'react';
-// import AddRecipeForm from 'components/AddRecipe/AddRecipeForm';
-// import Error from 'components/Error/Error';
-
-// const AddRecipePage = () => {
-//   const showError = true;
-
-//   return <div>{showError ? <Error /> : <AddRecipeForm />}</div>;
-// };
-
-// export default AddRecipePage;
