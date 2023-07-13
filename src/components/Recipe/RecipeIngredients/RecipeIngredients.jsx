e7f5aad3b6d7f414e9a99821f5f7f697ba4ed7d3
VladDyadenko <Stilmagroup@gmail.com>
2023-07-13 18:42:27 +0300

    added PresentModal

--- src/components/Recipe/RecipeIngredients/RecipeIngredients.jsx
+++ src/components/Recipe/RecipeIngredients/RecipeIngredients.jsx
@@ -22,9 +22,12 @@ import {
   selectShoppingList,
 } from 'redux/recipe/recipeSelector';
 import { addShoppingList, removeShoppingList } from 'redux/recipe/recipeSlice';
+import defaultFotoIngredient from '../../../images/Recipe/defaultFotoIngredient.png';
+import PresentModal from '../PresentModal/PresentModal';
 
 function RecipeIngredients({ ingredients }) {
   const [ingredientsList, setIngredientsList] = useState([]);
+  const [firstPurchase, setFirstPurchase] = useState(false);
 
   const allIngradientsList = useSelector(selectIngredients);
   const shoppingList = useSelector(selectShoppingList);
@@ -46,12 +49,16 @@ function RecipeIngredients({ ingredients }) {
   }, [ingredients, allIngradientsList]);
 
   const handleInputChange = e => {
+    if (shoppingList.length === 0) {
+      setFirstPurchase(true);
+    }
     const { id } = e.target;
     const currentIngredient = ingredientsList.find(val => val._id === id);
 
     const { _id, name, img, measure } = currentIngredient;
     const ingredientOnShoppingList = shoppingList.some(val => val.id === id);
     const ingredientForBuy = { measure, id: _id, name, img };
+
     if (!ingredientOnShoppingList) {
       dispatch(addShoppingList(ingredientForBuy));
     } else {
@@ -60,41 +67,47 @@ function RecipeIngredients({ ingredients }) {
   };
 
   return (
-    <ContainerRecipes>
-      <Container>
-        <IngradientsHeader info="Ingredients" actions="Add to list" />
-        <IngredientsWrapper>
-          {ingredientsList?.map(({ _id, name, img, measure }) => {
-            return (
-              <Ingredient key={_id}>
-                <WrapperContent>
-                  <ImageIngredient src={img} alt="Ingradient" />
-
-                  <IngredientName>{name}</IngredientName>
-                </WrapperContent>
-                <WrapperContent>
-                  <IngedientsMeasure>{measure}</IngedientsMeasure>
-
-                  <IngredientLabel htmlFor={_id}>
-                    <IngedientsInput
-                      type="checkbox"
-                      id={_id}
-                      checked={shoppingList.some(item => item.id === _id)}
-                      value={_id}
-                      onChange={handleInputChange}
+    <>
+      {firstPurchase && <PresentModal onClose={true} />}
+      <ContainerRecipes>
+        <Container>
+          <IngradientsHeader info="Ingredients" actions="Add to list" />
+          <IngredientsWrapper>
+            {ingredientsList?.map(({ _id, name, img, measure }) => {
+              return (
+                <Ingredient key={_id}>
+                  <WrapperContent>
+                    <ImageIngredient
+                      src={img ? img : defaultFotoIngredient}
+                      alt="Ingradient"
                     />
 
-                    <CheckContainer>
-                      <CheckIcon />
-                    </CheckContainer>
-                  </IngredientLabel>
-                </WrapperContent>
-              </Ingredient>
-            );
-          })}
-        </IngredientsWrapper>
-      </Container>
-    </ContainerRecipes>
+                    <IngredientName>{name}</IngredientName>
+                  </WrapperContent>
+                  <WrapperContent>
+                    <IngedientsMeasure>{measure}</IngedientsMeasure>
+
+                    <IngredientLabel htmlFor={_id}>
+                      <IngedientsInput
+                        type="checkbox"
+                        id={_id}
+                        checked={shoppingList.some(item => item.id === _id)}
+                        value={_id}
+                        onChange={handleInputChange}
+                      />
+
+                      <CheckContainer>
+                        <CheckIcon />
+                      </CheckContainer>
+                    </IngredientLabel>
+                  </WrapperContent>
+                </Ingredient>
+              );
+            })}
+          </IngredientsWrapper>
+        </Container>
+      </ContainerRecipes>
+    </>
   );
 }
 
