13dd66db97cbda84383165bd734d9e3e16279d6a
vitalij99 <vitalisklymko@gmail.com>
2023-07-13 17:39:15 +0300

    try

--- src/components/MainPage/PreviewCategories/PreviewCategories.jsx
+++ src/components/MainPage/PreviewCategories/PreviewCategories.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import CategoriesList from 'components/CategoriesList/CategoriesList';
@@ -8,12 +8,14 @@ import {
   WrapperStyled,
 } from './PreviewCategories.Styled';
 import { mainThunk } from 'redux/main/mainOperation';
-import { selectMainResipes } from 'redux/main/mainSelector';
+import { selectMainLoad, selectMainResipes } from 'redux/main/mainSelector';
 import Container from 'components/Container/Container';
 
 const PreviewCategories = () => {
   const list = useSelector(selectMainResipes);
-  const [viewList, setViewList] = useState(475);
+  const isLoad = useSelector(selectMainLoad);
+  const [view, setView] = useState(475);
+  const [listView, setListView] = useState([]);
   const dispath = useDispatch();
 
   useEffect(() => {
@@ -25,10 +27,10 @@ const PreviewCategories = () => {
       const screenWidth = window.innerWidth;
 
       if (screenWidth >= 1440) {
-        setViewList(list);
+        setView(1440);
       } else if (screenWidth >= 768) {
-        setViewList(list.splice(-1, 2));
-      } else setViewList(list[0]);
+        setView(768);
+      } else setView(475);
     };
 
     window.addEventListener('resize', handleResize);
@@ -38,6 +40,15 @@ const PreviewCategories = () => {
     };
   }, [list]);
 
+  const visibleTodos = useMemo(() => {
+    if (view >= 1440) {
+      const data = Object.keys(list).map(key => handleSlice(list.key));
+      console.log(data);
+      setListView(data);
+    } else if (view >= 768) {
+    } else;
+  }, []);
+
   const toCamelCase = str => {
     const words = str.split('_');
     const capitalizedWords = words.map(
@@ -46,15 +57,25 @@ const PreviewCategories = () => {
     return capitalizedWords.join('');
   };
 
+  const handleSlice = allList => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 1440) {
+      return allList;
+    } else if (screenWidth >= 768) {
+      return allList.slice(0, 2);
+    } else return [allList[0]];
+  };
+
   return (
     <>
       <Container>
         {list &&
+          !isLoad &&
           Object.keys(list).map(key => (
             <div key={key}>
               <TitleStyled>{toCamelCase(key)}</TitleStyled>
               <WrapperStyled>
-                <CategoriesList list={viewList[key]} />
+                <CategoriesList list={handleSlice(list[key])} />
                 <LinkStyled to={`/categories/${toCamelCase(key)}`}>
                   See all
                 </LinkStyled>
