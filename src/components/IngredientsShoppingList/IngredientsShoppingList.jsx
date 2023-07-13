1152df55fd6ffd34975846c2fa5141e6bb2f4438
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-07-14 00:23:13 +0300

    shopping-list-connected

--- src/components/IngredientsShoppingList/IngredientsShoppingList.jsx
+++ src/components/IngredientsShoppingList/IngredientsShoppingList.jsx
@@ -1,24 +1,29 @@
 import Container from 'components/Container/Container';
-import React from 'react'; // useEffect
+import React from 'react';
 import { Section } from './IngredientsShoppingList.styled';
 import { ListSection } from './ListSection';
 
 import { TitlesSection } from './TitlesSection';
-import // useDispatch,
-// useSelector,
-'react-redux';
-// import { selectShoppingList } from 'redux/recipe/recipeSelector';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectShoppingList } from 'redux/recipe/recipeSelector';
+import { removeShoppingList } from 'redux/recipe/recipeSlice';
 
 export const IngredientsShoppingList = () => {
-  // const dispatch = useDispatch();
-  // const shoppingList = useSelector(selectShoppingList);
+  const dispatch = useDispatch();
+
+  const shoppingList = useSelector(selectShoppingList);
+
+  const handleRemoveShoppingList = id => {
+    // console.log(id);
+    dispatch(removeShoppingList(id));
+  };
 
   return (
     <>
       <Section>
         <Container>
           <TitlesSection />
-          <ListSection data={[]} />
+          <ListSection data={shoppingList} onClick={handleRemoveShoppingList} />
         </Container>
       </Section>
     </>
---
+++ src/components/Search/SearchForm/SearchFrom.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SearchFrom = () => {
+  return <div>SearchFrom</div>;
+};
