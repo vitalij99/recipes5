13c478c56ec358f984fd8a58b7aee0c0d22e40e2
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-07-14 04:50:06 +0300

    search-form

--- src/components/Search/SearchForm/SearchFrom.jsx
+++ src/components/Search/SearchForm/SearchFrom.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
 
-export const SearchFrom = () => {
-  return <div>SearchFrom</div>;
+export const SearchForm = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log('Search term:', searchTerm);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
+        placeholder="Enter search term"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
 };
