0ed590dea6bd384964d2383aea9a4b892ace8a11
NVZ1994 <110624272+NVZ1994@users.noreply.github.com>
2023-07-17 12:51:36 +0000

    Deploying to gh-pages from @ vitalij99/recipes5@7b00dfd542bc8f1aeb2f7e2a34a9b4d46a390ccb 🚀

---
+++ static/js/925.84db45d1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrecipes5=self.webpackChunkrecipes5||[]).push([[925],{2925:function(e,s,c){c.r(s);var n=c(5626),r=(c(2791),c(3329));s.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)(n.Z,{})})}}}]);
+//# sourceMappingURL=925.84db45d1.chunk.js.map
