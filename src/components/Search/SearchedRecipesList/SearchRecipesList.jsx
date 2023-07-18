100b329d5a3706ac913e26abe60a5f786ef11060
vitalij99 <vitalisklymko@gmail.com>
2023-07-18 13:48:03 +0300

    add search

--- src/components/Search/SearchedRecipesList/SearchRecipesList.jsx
+++ src/components/Search/SearchedRecipesList/SearchRecipesList.jsx
@@ -1,16 +1,48 @@
+/* eslint-disable no-unused-vars */
 import { CardItem } from 'components/Categories/CardItem';
 import Container from 'components/Container/Container';
 import { Section } from 'components/Search/SearchedRecipesList/SearchRecipesList.styled';
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import data from '../../Categories/data.json';
 import { SearchNotFound } from '../SearchNotFound/SearchNotFound';
+import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
 
 export const SearchRecipesList = () => {
+  const [searchParams] = useSearchParams();
+  const [listRes, setListRes] = useState([]);
+  const params = useMemo(
+    () => Object.fromEntries([...searchParams]),
+    [searchParams]
+  );
+  useEffect(() => {
+    const { query, ingredient } = params;
+
+    const fetchData = async () => {
+      try {
+        if (query) {
+          const response = await axios.post('recipes/search', {
+            title: query,
+          });
+          setListRes(response);
+        } else if (ingredient) {
+          const response = await axios.post('recipes/search', {
+            ingredient,
+          });
+          setListRes(response);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <Section>
         <Container>
-          {data ? <SearchNotFound /> : <CardItem data={data} />}
+          {listRes.length === 0 ? <SearchNotFound /> : <CardItem data={data} />}
         </Container>
       </Section>
     </>
