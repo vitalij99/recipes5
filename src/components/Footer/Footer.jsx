701ca8ed014a2eb8782504ede03c5b79e551fd0f
VladDyadenko <Stilmagroup@gmail.com>
2023-07-21 16:04:50 +0300

    added teamModal

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -30,85 +30,103 @@ import { FooterLogo, FooterEmailLogo } from './FooterIcon';
 
 import Container from 'components/Container/Container';
 import { StyledSocialIcons } from './SocialIcons/SocialIcons.styled';
+import { useState } from 'react';
+import ModalTeam from 'components/ModalTeam/ModalTeam';
 
 const Footer = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  function handleModalTeam(e) {
+    setIsOpenModal(true);
+  }
+
   return (
-    <BackContainer>
-      <Container>
-        <FooterInfo>
-          <MainContainer>
-            <TextContainer>
-              <HeadContainer>
-                <FooterLogoContainer>
-                  <LogoWrapper to="/">
-                    <FooterLogo />
-                  </LogoWrapper>
+    <>
+      {isOpenModal && <ModalTeam onClose={isOpenModal} />}
+      <BackContainer>
+        <Container>
+          <FooterInfo>
+            <MainContainer>
+              <TextContainer>
+                <HeadContainer>
+                  <FooterLogoContainer>
+                    <LogoWrapper to="/">
+                      <FooterLogo />
+                    </LogoWrapper>
 
-                  <FooterLogoText>So Yummy</FooterLogoText>
-                </FooterLogoContainer>
+                    <FooterLogoText>So Yummy</FooterLogoText>
+                  </FooterLogoContainer>
 
-                <OptionsList>
-                  <OptionsItem>
-                    Database of recipes that can be replenished{' '}
-                  </OptionsItem>
-                  <OptionsItem>
-                    Flexible search for desired and unwanted ingredients
-                  </OptionsItem>
-                  <OptionsItem>
-                    Ability to add your own recipes with photos
-                  </OptionsItem>
-                  <OptionsItem>Convenient and easy to use</OptionsItem>
-                </OptionsList>
-              </HeadContainer>
+                  <OptionsList>
+                    <OptionsItem>
+                      Database of recipes that can be replenished
+                    </OptionsItem>
+                    <OptionsItem>
+                      Flexible search for desired and unwanted ingredients
+                    </OptionsItem>
+                    <OptionsItem>
+                      Ability to add your own recipes with photos
+                    </OptionsItem>
+                    <OptionsItem>Convenient and easy to use</OptionsItem>
+                  </OptionsList>
+                </HeadContainer>
 
-              <NavLinkDiv>
-                <NavLinkList>
-                  <NavLinkItem>
-                    <LinkStyle to={'/search?ingredient'}>Ingredients</LinkStyle>
-                  </NavLinkItem>
-                  <NavLinkItem>
-                    <LinkStyle to={'/add'}>Add recipes</LinkStyle>
-                  </NavLinkItem>
-                  <NavLinkItem>
-                    <LinkStyle to={'/recipe/:recipeId'}>My recipes</LinkStyle>
-                  </NavLinkItem>
-                  <NavLinkItem>
-                    <LinkStyle to={'/favorite'}>Favorites</LinkStyle>
-                  </NavLinkItem>
-                  <NavLinkItem>
-                    <LinkStyle to={'/shopping-list'}>Shopping list</LinkStyle>
-                  </NavLinkItem>
-                </NavLinkList>
-              </NavLinkDiv>
-            </TextContainer>
+                <NavLinkDiv>
+                  <NavLinkList>
+                    <NavLinkItem>
+                      <LinkStyle to={'/search?ingredient'}>
+                        Ingredients
+                      </LinkStyle>
+                    </NavLinkItem>
+                    <NavLinkItem>
+                      <LinkStyle to={'/add'}>Add recipes</LinkStyle>
+                    </NavLinkItem>
+                    <NavLinkItem>
+                      <LinkStyle to={'/recipe/:recipeId'}>My recipes</LinkStyle>
+                    </NavLinkItem>
+                    <NavLinkItem>
+                      <LinkStyle to={'/favorite'}>Favorites</LinkStyle>
+                    </NavLinkItem>
+                    <NavLinkItem>
+                      <LinkStyle to={'/shopping-list'}>Shopping list</LinkStyle>
+                    </NavLinkItem>
+                  </NavLinkList>
+                </NavLinkDiv>
+              </TextContainer>
 
-            <SubscribeContainer>
-              <SubscribeTitle>Subscribe to our Newsletter</SubscribeTitle>
-              <SubscribeText>
-                Subscribe up to our newsletter. Be in touch with latest news and
-                special offers, etc.
-              </SubscribeText>
+              <SubscribeContainer>
+                <SubscribeTitle>Subscribe to our Newsletter</SubscribeTitle>
+                <SubscribeText>
+                  Subscribe up to our newsletter. Be in touch with latest news
+                  and special offers, etc.
+                </SubscribeText>
 
-              <Form action="subscribe">
-                <InputBox>
-                  <ContainerSvg>
-                    <FooterEmailLogo />
-                  </ContainerSvg>
-                  <Input type="email" placeholder="Enter your email address" />
-                </InputBox>
-                <FooterButton type="submit">Subscribe</FooterButton>
-              </Form>
-            </SubscribeContainer>
-          </MainContainer>
-          <StyledSocialIcons />
-        </FooterInfo>
-      </Container>
+                <Form action="subscribe">
+                  <InputBox>
+                    <ContainerSvg>
+                      <FooterEmailLogo />
+                    </ContainerSvg>
+                    <Input
+                      type="email"
+                      placeholder="Enter your email address"
+                    />
+                  </InputBox>
+                  <FooterButton type="submit">Subscribe</FooterButton>
+                </Form>
+              </SubscribeContainer>
+            </MainContainer>
+            <StyledSocialIcons />
+          </FooterInfo>
+        </Container>
 
-      <BottomFooter>
-        <Copyright>© 2023 All Rights Reserved.</Copyright>
-        <TermsofService>Terms of Service</TermsofService>
-      </BottomFooter>
-    </BackContainer>
+        <BottomFooter>
+          <Copyright>© 2023 All Rights Reserved.</Copyright>
+          <TermsofService onClick={handleModalTeam}>
+            Terms of Service
+          </TermsofService>
+        </BottomFooter>
+      </BackContainer>
+    </>
   );
 };
 
---
+++ src/components/ModalTeam/TeamInfo.js
@@ -0,0 +1,81 @@
+export const developers = [
+  {
+    name: 'Vitalij',
+    surname: 'Kymko',
+    gitLink: 'https://github.com/vitalij99',
+    foto: 'https://avatars.githubusercontent.com/u/111449205?v=4',
+  },
+  {
+    name: 'Dmytro',
+    surname: 'Kornilov',
+    gitLink: 'https://github.com/Dimar95',
+    foto: 'https://avatars.githubusercontent.com/u/110562214?v=4',
+  },
+  {
+    name: 'Borys',
+    surname: 'Romanov',
+    gitLink: 'https://github.com/Umberban',
+    foto: 'https://avatars.githubusercontent.com/u/110768888?v=4',
+  },
+  {
+    name: 'Anastasiia',
+    surname: 'Herusova',
+    gitLink: 'https://github.com/Anastasiia2302',
+    foto: 'https://avatars.githubusercontent.com/u/110552981?v=4',
+  },
+  {
+    name: 'Vladimir',
+    surname: 'Diadenko',
+    gitLink: 'https://github.com/VladDyadenko',
+    foto: 'https://avatars.githubusercontent.com/u/110708917?v=4',
+  },
+  {
+    name: 'Yanna',
+    surname: 'Trofimova',
+    gitLink: 'https://github.com/Yanna-Trofimova',
+    foto: 'https://avatars.githubusercontent.com/u/111576903?v=4',
+  },
+  {
+    name: 'Anastasiia',
+    surname: 'Vakarchuk',
+    gitLink: 'https://github.com/Vastasika',
+    foto: 'https://avatars.githubusercontent.com/u/111005415?v=4',
+  },
+  {
+    name: 'Nataliia',
+    surname: 'Malakova',
+    gitLink: 'https://github.com/natalynoele',
+    foto: 'https://avatars.githubusercontent.com/u/46425788?v=4',
+  },
+  {
+    name: 'Yaroslav',
+    surname: 'Velichko',
+    gitLink: 'https://github.com/BaristaYaroslv',
+    foto: 'https://avatars.githubusercontent.com/u/110727630?v=4',
+  },
+  {
+    name: 'Andrii',
+    surname: 'Kryzhanivskyi',
+    gitLink: 'https://github.com/Kryzhanivskyi89',
+    foto: 'https://avatars.githubusercontent.com/u/111448776?v=4',
+  },
+
+  {
+    name: 'Anastasiia',
+    surname: 'Bakumenko',
+    gitLink: 'https://github.com/AnastasiiaBakumenko',
+    foto: 'https://avatars.githubusercontent.com/u/111183401?v=4',
+  },
+  {
+    name: 'Igor',
+    surname: 'Berezhnoy',
+    gitLink: 'https://github.com/webdevbiv',
+    foto: 'https://avatars.githubusercontent.com/u/80968651?v=4',
+  },
+  {
+    name: 'Nikita',
+    surname: 'Zhuk',
+    gitLink: 'https://github.com/NVZ1994',
+    foto: 'https://avatars.githubusercontent.com/u/110624272?v=4',
+  },
+];
