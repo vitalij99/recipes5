8ac2c05b56ccaa6553c7cd97e6de37ac4c242ce2
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-07-21 05:09:37 +0300

    new-shadow

--- src/components/Categories/Categories.styled.jsx
+++ src/components/Categories/Categories.styled.jsx
@@ -99,9 +99,8 @@ export const CategoriesImgWrap = styled.div`
   &:focus {
     transform: scale(1.05);
     outline: 1.5px solid ${props => props.theme.mainPage.menuMobileModal.hover};
-    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
-      rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
-      rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+    box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px,
+      rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
 
     transition: 0.6s;
   }
